1f5683aefe03ce494596fe0324765575a278d6b6
ravindaa <ravindaa@oregonstate.edu>
2022-12-18 21:39:20 +0100

    Final validation example

--- example_1.py
+++ example_1.py
@@ -1,28 +1,44 @@
-import tkinter
+import tkinter as tk
 from tkinter import *
-  
-  
-def callback(input):
-      
-    if input.isdigit():
-        print(input)
-        return True
-                          
-    elif input is "":
-        print(input)
-        return True
-  
+my_w = tk.Tk()
+import re
+
+
+my_w.geometry("400x100")  # Size of the window 
+my_w.title("www.plus2net.com")  # Adding a title
+
+def validate(u_input):
+    vl.config(text="")
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    if(re.search(regex,u_input) and u_input.isalpha):
+        print(True)
+        b1.config(state='active')  
+        return True        
     else:
-        print(input)
-        return False
-                          
-root = Tk()
-  
-e = Entry(root)
-e.place(x = 50, y = 50)
-reg = root.register(callback)
-  
-e.config(validate ="key", 
-         validatecommand =(reg, '% P'))
-  
-root.mainloop()
+        vl.config(text="Name or email invalid", foreground="red")
+        print(False)
+        b1.config(state='disabled')  
+        return False  
+   
+my_valid = my_w.register(validate)
+
+vl = tk.Label(my_w)
+vl.grid(row=2, column=2)
+
+l1=tk.Label(my_w,text='Email')
+l1.grid(row=1,column=1,padx=5,pady=20)
+e1 = Entry(my_w,validate='focusout',validatecommand=(my_valid,'%P'))
+e1.grid(row=1,column=2,padx=10)
+
+
+l2=tk.Label(my_w,text='Name')
+l2.grid(row=1,column=3,padx=5,pady=20)
+e2 = tk.Entry(my_w,width=10)
+e2.grid(row=1,column=4)
+
+
+b1 = tk.Button(my_w,text='Submit')
+b1.grid(row=1,column=5)
+
+my_w.mainloop()  # Keep the window open
+print(type(4.5))
